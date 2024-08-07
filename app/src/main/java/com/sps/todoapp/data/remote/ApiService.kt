53151950f50e60a33028a2496bdf6ded8208fdd0
Dilip Patel <dilipp817@gmail.com>
2024-08-08 01:08:16 +0530

    retrofit setup

---
+++ app/src/main/java/com/sps/todoapp/data/remote/ApiService.kt
@@ -0,0 +1,5 @@
+package com.sps.todoapp.data.remote
+
+interface ApiService {
+    //app interacts to database only, no api call required
+}
--- app/src/main/java/com/sps/todoapp/di/module/ApplicationModule.kt
+++ app/src/main/java/com/sps/todoapp/di/module/ApplicationModule.kt
@@ -1,10 +1,55 @@
 package com.sps.todoapp.di.module
 
+import com.sps.todoapp.data.remote.ApiHelper
+import com.sps.todoapp.data.remote.ApiHelperImpl
+import com.sps.todoapp.data.remote.ApiService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class ApplicationModule {
+
+    @Provides
+    fun provideBaseUrl() = "https://5e510330f2c0d300147c034c.mockapi.io/"
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() = if (true) {  //TODO replace true with BuildConfig.DEBUG
+        val loggingInspector = HttpLoggingInterceptor()
+        loggingInspector.level = HttpLoggingInterceptor.Level.BODY
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInspector)
+            .build()
+    }
+    else {
+        OkHttpClient.Builder()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        BASE_URL: String
+    ): Retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideApiHelper(apiHelper: ApiHelperImpl): ApiHelper = apiHelper
 }
